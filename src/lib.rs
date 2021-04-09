66e57f7d27ee8c6299f7c61722d49956689513a0
Henry de Valence <hdevalence@hdevalence.ca>
2021-04-09 09:39:17 -0700

    fix up docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#![cfg_attr(feature = "doc", feature(extended_key_value_attributes))]
-#![cfg_attr(feature = "doc", doc = include_str!("../README.md"))]
+#![cfg_attr(feature = "doc", feature(external_doc))]
+#![cfg_attr(feature = "doc", doc(include = "../README.md"))]
 
 /// ABCI requests.
 pub mod request;
