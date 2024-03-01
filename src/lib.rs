1cf2085f5bdb17aed3f6972013fde853b84e3133
Erwan Or <erwan.ounn.84@gmail.com>
2024-03-01 09:11:09 -0500

    tower-abci: support CometBFT `0.38.x` (#42)

--- src/lib.rs
+++ src/lib.rs
@@ -21,5 +21,13 @@ pub mod v037 {
     pub use server::ServerBuilder;
 }
 
+pub mod v038 {
+    mod codec;
+    mod server;
+    pub mod split;
+    pub use server::Server;
+    pub use server::ServerBuilder;
+}
+
 /// A convenient error type alias.
 pub type BoxError = Box<dyn std::error::Error + Send + Sync + 'static>;
